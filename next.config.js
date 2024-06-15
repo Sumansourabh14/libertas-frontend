8b52c55f3929a7da66004d146a8b0b62a9e40194
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-06-15 12:50:43 +0530

    added mdx to create a blog

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const withMDX = require("@next/mdx")({});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ["wallpapers.com", "firebasestorage.googleapis.com"],
   },
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);
