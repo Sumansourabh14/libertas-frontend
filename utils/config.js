47ea9613f941db1be1415d3dd70176e325d3fc88
Suman Sourabh <suman.sourabh@ansrsource.com>
2024-06-22 18:38:09 +0530

    updated api url to live

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
-// export const API_URL = `https://libertas-backend.onrender.com`;
-export const API_URL = `http://localhost:7001`;
+export const API_URL = `https://libertas-backend.onrender.com`;
+// export const API_URL = `http://localhost:7001`;
 export const QUOTES_API_URL = `https://recite.onrender.com`;
